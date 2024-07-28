fd6018aa6e8318ba151272a5d54c4c20d2e8044c
SatyamPathak12 <satyampathak08122000k@gmail.com>
2024-07-28 14:57:36 +0530

    create frontend for signin

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import SignIn from './pages/Signin'
 import Dashboard from './pages/Dashboard'
 import SignUp from './pages/Signup'
 import Header from './components/Header'
 import FooterComponent from './components/Footer'
+import SignIn from './pages/Signin'
+import { Footer } from 'flowbite-react'
 
 export default function App() {
   return (
@@ -15,7 +16,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<h1>Home</h1>}></Route>
         <Route path="/about" element={<h1>About</h1>}></Route>
-        <Route path="/signin" element={<h1>SignIn</h1>}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/projects" element={<h1>Projects</h1>}></Route>
         <Route path="/dashboard" element={<h1>Dashboard</h1>}></Route>
