b8d89d77a92630fa68f2cded570349f76b31cfd0
SatyamPathak12 <satyampathak08122000k@gmail.com>
2024-08-17 15:42:54 +0530

    update user api route

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,10 @@ import dotenv from 'dotenv';
 import cors from 'cors'; 
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
+
+
 dotenv.config();
 
 
@@ -36,7 +40,7 @@ app.use((err, req, res, next) => {
         message,
     });
 });
-
+app.use(cookieParser());
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
