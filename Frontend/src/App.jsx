b28f44986229513c434a8648b644d54b6ffce3e1
SatyamPathak12 <satyampathak08122000k@gmail.com>
2024-09-09 10:44:57 +0530

    complete post functionallity

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 
 export default function App() {
   return (
@@ -33,6 +34,8 @@ export default function App() {
         </Route>
 
         <Route path="/projects" element={<h1>Projects</h1>} />
+        <Route path="/post/:postSlug" element={<PostPage/>} />
+
       </Routes>
       <FooterComponent />
     </BrowserRouter>
